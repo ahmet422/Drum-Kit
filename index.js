1d6e215cc147357fe94e0f7477f0e023bf1d4500
ahmet422 <ahmet.tajik@icloud.com>
2020-10-08 10:07:23 -0500

    added js file and transferred script code into the file

---
+++ index.js
@@ -0,0 +1,19 @@
+function playsound(e){
+    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+    const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+   
+    if(!audio) return;
+    audio.currentTime = 0; // rewind to the start
+    audio.play();
+    key.classList.add('playing');
+  }
+
+  function removeTransition(e){
+  //a  console.log(propertyName);
+    if(e.propertyName !== 'transform') return; // skip it if its not transform
+    this.classList.remove('playing');
+  }
+
+  const keys = document.querySelectorAll('.key');
+  keys.forEach(k => k.addEventListener('transitionend', removeTransition));
+  window.addEventListener('keydown', playsound);
